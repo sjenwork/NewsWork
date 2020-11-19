03bc6008752efe0fa86e4a75813e74873805a0cd
sjenwork <sjenwork@gmail.com>
2020-11-19 12:05:10 +0800

    add new rule

---
+++ Crawler/bat/C.US_fda.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set curdir=S:\TV\N332-NewsWork\Crawler
+
+cd /d %curdir%
+
+set news=美國食藥局
+set datestr=%date:~0,4%-%date:~5,2%-%date:~8,2%
+set log=getNews_%datestr%_%news%.log
+set err=getNews_%datestr%_%news%.err
+
+echo 抓取新聞資料中，log存放於 DATA/log/%log%
+echo 當前目錄：%curdir%
+
+python getEngNews.py %datestr% %news% > DATA/log/%log% 2>DATA/log/%err%
+
